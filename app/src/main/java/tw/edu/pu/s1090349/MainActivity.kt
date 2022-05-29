acdc74dfca85aa65f3736fdc0f9ffcd2e6e4b392
s1090349 <s1090349@gm.pu.edu.tw>
2022-05-29 20:51:57 +0800

    v.3

--- app/src/main/java/tw/edu/pu/s1090349/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/s1090349/MainActivity.kt
@@ -4,10 +4,15 @@ import android.graphics.Canvas
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
 import kotlinx.coroutines.*
 import kotlinx.android.synthetic.main.activity_main.*
 import tw.edu.pu.s1090349.databinding.ActivityMainBinding
 
+@GlideModule
+public final class MyAppGlideModule : AppGlideModule()
 class MainActivity : AppCompatActivity(){
 
     lateinit var binding: ActivityMainBinding
@@ -42,7 +47,12 @@ class MainActivity : AppCompatActivity(){
                     delay(25)
                 }
             }
-
+            val img: ImageView = findViewById(R.id.img2)
+            GlideApp.with(this)
+                .load(R.drawable.picture)
+                .circleCrop()
+                .override(800, 600)
+                .into(img2)
             //設定螢幕水平顯示
             setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE)
 
